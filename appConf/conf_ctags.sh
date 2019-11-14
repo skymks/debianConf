9675577b6529549c9dac003f72891ed9c90acb50
jqx <skymks@foxmail.com>
2019-11-14 14:43:16 +0800

    ADD

---
+++ appConf/conf_ctags.sh
@@ -0,0 +1,3 @@
+#初始化系统库标签
+ctags -R --c-kinds=+l+x+p --fields=+lS -I __THROW,__nonnull
+
--- appConf/install.sh
+++ appConf/install.sh
@@ -4,6 +4,3 @@
 apt-get install proxychains
 #ctag
 apt-get install exuberant-ctags 
-
-#初始化系统库标签
-ctags -R --c-kinds=+l+x+p --fields=+lS -I __THROW,__nonnull
